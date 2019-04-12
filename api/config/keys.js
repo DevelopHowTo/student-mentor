b8088b9abaaa913b006ab2f9820e43ca2b802352
shivam123425 <shivam123425@gmail.com>
2019-04-12 23:50:08 +0530

    Add keys.js

---
+++ api/config/keys.js
@@ -0,0 +1,10 @@
+module.exports = {
+  secretOrkey: "secret",
+  mailerUserName: "<gmail username>",
+  mailerPass: "<gmail password>",
+  mailerFrom: '"<email id>" <@gmail.com>',
+  mailerService: "Gmail",
+  cloudinary_cloud_name: "dpumghgnd",
+  cloudinary_api_key: "964254149445422",
+  cloudinary_api_secret: "NcRkETyCmbWKgBBeTK_uBHLBcCo"
+};
