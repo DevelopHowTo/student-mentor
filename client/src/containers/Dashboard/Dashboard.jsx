9f12ca17a4b2405ca45b91badd56aa3e1dafa02b
shivam123425 <shivam123425@gmail.com>
2019-04-13 16:04:12 +0530

    Handle logout

--- client/src/containers/Dashboard/Dashboard.jsx
+++ client/src/containers/Dashboard/Dashboard.jsx
@@ -1,9 +1,31 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import * as actionTypes from "../../store/actions/actionTypes";
 
 class Dashboard extends Component {
+  handleLogout = () => {
+    // Remove the token from local storage
+    localStorage.removeItem("token");
+    // Dispatch logout action
+    this.props.logout();
+  };
   render() {
-    return <div>Dashboard</div>;
+    return (
+      <div>
+        Dashboard
+        <button onClick={this.handleLogout}>Logout</button>
+      </div>
+    );
   }
 }
 
-export default Dashboard;
+const mapDispatchToProps = dispatch => {
+  return {
+    logout: () => dispatch({ type: actionTypes.LOGOUT })
+  };
+};
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(Dashboard);
